8ccef080a8503778bc95cf690aa65bd5850d9d8e
Dzima Bykau <bykovdmitry3@gmail.com>
2023-12-20 13:59:39 +0300

    feat: solve carbon-dating task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,11 +17,16 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
+function dateSample(sampleActivity) {
   // remove line with error and write your code here
+  if (typeof sampleActivity !== 'string' || !sampleActivity || !Boolean(Number(sampleActivity))) {
+    return false;
+  }
+  
+  const k = 0.693 / HALF_LIFE_PERIOD;
+  const t = Math.ceil(Math.log(MODERN_ACTIVITY / Number(sampleActivity)) / k);
+  return t > 0 ? t : false;
 }
-
 module.exports = {
   dateSample
 };
