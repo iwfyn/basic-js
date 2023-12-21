f58c38f1c64a35d80cfe69c52621718b794aeb04
Dzima Bykau <bykovdmitry3@gmail.com>
2023-12-21 10:37:24 +0300

    feat: solve dns-stats task

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,25 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  let result = new Object();
+  let domainsArr = domains.map(el => el.split('.').reverse());
+  console.log(domainsArr)
+  result = domainsArr.reduce(((acc, el) => {
+      // Накапливаем информацию в новом объекте
+      let currDomain = new String(); // Префикс первого объекта в массиве будет пустой строкой
+      for (let i = 0; i < el.length; i += 1) {
+          // Проходимся по каждому из элементов первоначального массива
+          currDomain = `${currDomain}.${el[i]}`; // Создаем переменную, в которую будут добавлены элементы из строчек массива. При первом проходе в строке будет только .+корневойДомен
+          if (currDomain in acc) {
+              acc[currDomain] = acc[currDomain] + 1;
+          } else {
+              acc[currDomain] = 1;
+          }
+      }
+      return acc;
+  }), {});
+  return result;
 }
 
 module.exports = {
