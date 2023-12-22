494038a30aecf57603cfbb22b0805e7cf88ae03d
Dzima Bykau <bykovdmitry3@gmail.com>
2023-12-22 12:31:34 +0300

    feat: solve dream-team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  let res = [];
+  if (Array.isArray(members)) {
+      for (let i = 0; i < members.length; i += 1) {
+          if (typeof members[i] === 'string') {
+              res.push(members[i].trim().slice(0, 1).toUpperCase());
+          }
+      }
+      return res.sort((a, b) => a.localeCompare(b)).join('');
+  }
+  return false;
 }
 
 module.exports = {
